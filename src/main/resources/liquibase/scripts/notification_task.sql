7c511f7a9b93dd5e3d77bd40aa6d99c11465b137
Nikita <athlon64x2600@gmail.com>
2024-02-20 15:18:58 +0200

    Добавил liquibase

---
+++ src/main/resources/liquibase/scripts/notification_task.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset postgres:1
+//CREATE TABLE notification_task
+//(
+    //notification_id bigserial primary key,
+    //chat_id bigserial,
+    //notification_text varchar,
+    //notification_date timestamp
+//);
